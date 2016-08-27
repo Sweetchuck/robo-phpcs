bffd42b216d1a83e996b3fbf0ea8e5c198b65984
Andor Dávid <andor.david@cheppers.com>
2016-08-28 00:27:48 +0200

    Update dependencies

--- RoboFile.php
+++ RoboFile.php
@@ -126,7 +126,9 @@ class RoboFile extends \Robo\Tasks
             $cmd_args[] = escapeshellarg("{$this->binDir}/codecept");
         }
 
-        $cmd_pattern .= ' --ansi --coverage --coverage-xml --coverage-html=html run';
+        $cmd_pattern .= ' --ansi --verbose --coverage --coverage-xml=%s --coverage-html=%s run';
+        $cmd_args[] = escapeshellarg('coverage.xml');
+        $cmd_args[] = escapeshellarg('html');
 
         return $this
           ->taskExec(vsprintf($cmd_pattern, $cmd_args))
